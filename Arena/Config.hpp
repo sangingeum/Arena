c52e959c825e6aefa80d9600d12fbd678b4b1833
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-11-05 23:41:38 +0900

    Add config values
    Add config values

--- Arena/Config.hpp
+++ Arena/Config.hpp
@@ -5,7 +5,9 @@
 // It stores configuration related values and some constants
 class Config
 {
-	Config() = default;
+	Config()
+		:halfWindowWidth(windowWidth / 2.f), halfWindowHeight(widowHeight / 2.f)
+	{}
 	Config(const Config&) = delete;
 	void operator=(const Config&) = delete;
 	~Config() = default;
@@ -18,6 +20,10 @@ public:
 	uint32_t frameRate{ 60 };
 	int windowWidth{ 1280 };
 	int widowHeight{ 720 };
+	float halfWindowWidth;
+	float halfWindowHeight;
+	int32 velocityIterations{ 16 };
+	int32 positionIterations{ 6 };
 	const float pi{ 3.14159265358979323846f };
 	const float screenToWorldRatio{ 0.01f };
 	const float radianToDegree{ 57.2958f };
