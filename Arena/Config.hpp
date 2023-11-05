f25b6bafa34c39bdccc6c465ef3986fb876722e3
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-11-05 21:26:48 +0900

    Implement game system
    Implement game system

--- Arena/Config.hpp
+++ Arena/Config.hpp
@@ -2,9 +2,10 @@
 #include <SFML/Graphics.hpp>
 
 // Singleton config
+// It stores configuration related values and some constants
 class Config
 {
-	Config() {}
+	Config() = default;
 	Config(const Config&) = delete;
 	void operator=(const Config&) = delete;
 	~Config() = default;
@@ -14,10 +15,12 @@ public:
 		static Config config;
 		return config;
 	}
-	unsigned frameRate{ 60 };
-	int windowWidth{ 1200 };
-	int widowHeight{ 700 };
+	uint32_t frameRate{ 60 };
+	int windowWidth{ 1280 };
+	int widowHeight{ 720 };
 	const float pi{ 3.14159265358979323846f };
+	const float screenToWorldRatio{ 0.01f };
+	const float radianToDegree{ 57.2958f };
 	sf::String windowName{ "Arena" };
 };
 
--- Arena/GameSystem.hpp
+++ Arena/GameSystem.hpp
@@ -8,6 +8,8 @@
 #include "Scene.hpp"
 #include "SceneMenu.hpp"
 #include "SceneLoader.hpp"
+#include "ActionBinder.hpp"
+
 class GameSystem
 {
 	using SceneMap = std::map<SceneID, std::shared_ptr<Scene>>;
@@ -21,26 +23,33 @@ public:
 	GameSystem()
 		: m_config(Config::instance()), m_window(sf::VideoMode(m_config.windowWidth, m_config.widowHeight), m_config.windowName)
 	{
-		m_window.setFramerateLimit(m_config.frameRate);
 		init();
 	}
 	~GameSystem() = default;
 	void run() {
 		while (m_window.isOpen()) {
 			sUpdate();
+			sPhysics();
 			sHandleInput();
 			sRender();
 		}
 	}
 	void quit() {
-
+		m_window.close();
 	}
 
 private:
 	void init() {
+		m_window.setFramerateLimit(m_config.frameRate);
+		float winHalfWidth = m_config.windowWidth / 2.f;
+		float winHalfHeight = m_config.widowHeight / 2.f;
+		auto curView = m_window.getView();
+		curView.setCenter({ winHalfWidth * m_config.screenToWorldRatio, winHalfHeight * m_config.screenToWorldRatio });
+		curView.zoom(m_config.screenToWorldRatio);
+		m_window.setView(curView);
 		changeScene(SceneID::mainMenu, false, true);
 	}
-	std::shared_ptr<Scene> getCurrentScene() {
+	inline std::shared_ptr<Scene> getCurrentScene() {
 		return m_sceneMap[m_currentSceneID];
 	}
 	void changeScene(SceneID id, bool destroyCurrentScene, bool overwriteIfExists) {
@@ -56,13 +65,21 @@ private:
 		getCurrentScene()->sUpdate();
 	}
 	void sHandleInput() {
-
+		sf::Event event;
+		while (m_window.pollEvent(event))
+		{
+			Action action = ActionBinder::getAction(event);
+			getCurrentScene()->sHandleAction(std::move(action));
+		}
+	}
+	void sPhysics() {
+		getCurrentScene()->sPhysics();
 	}
 	void sRender() {
 		m_window.clear();
-
-
+		getCurrentScene()->sRender(m_window);
 		m_window.display();
 	}
+
 };
 
